bc6f7dfd38e5890959d1ed257d5c23cb2a075e82
GlinkaYulia <gsvfdgazi@gmail.com>
2026-01-26 18:46:01 +0200

    Add navigation fragments

---
+++ app/src/main/java/com/test/furnitureapp/fragment/FavoriteFragment.kt
@@ -0,0 +1,13 @@
+package com.test.furnitureapp.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.test.furnitureapp.R
+
+class FavoriteFragment : Fragment(R.layout.fragment_favorite) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+}
